3dc46115d77a03950c5df64ad04d78b4a3af6cc7
Jen-Chieh Shen <jcs090218@gmail.com>
2022-01-04 00:08:59 +0800

    Update copyright year

--- hl-preproc.el
+++ hl-preproc.el
@@ -1,6 +1,6 @@
 ;;; hl-preproc.el --- Unhighlight invalid preprocessor region  -*- lexical-binding: t; -*-
 
-;; Copyright (C) 2021  Shen, Jen-Chieh
+;; Copyright (C) 2021-2022  Shen, Jen-Chieh
 ;; Created date 2021-07-03 17:07:11
 
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
